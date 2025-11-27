f086ccb31332cec5333a3abf259f0f137d1a5aef
Martin Doulík <123932205+robofancz@users.noreply.github.com>
2025-11-27 18:21:43 +0100

    [EP 27 - New project]
    Design and Blade

---
+++ pixel-positions/resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="flex flex-col p-4 bg-white/5 rounded-xl text-center h-full">
+    <div class="text-start text-sm">Employer</div>
+
+    <div class="py-8 font-bold">
+        <h3 class="mb-6 truncate">Title</h3>
+        <p>Salary</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div class="flex gap-2">
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            
+        </div>
+        <img src="https://placehold.co/42x42" alt="" class="rounded">
+    </div>
+</div>
